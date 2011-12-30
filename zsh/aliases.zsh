d72db500a3f3cc86dece4904a5334cc65198ced4
Cormac Relf <chjrelf@gmail.com>
2011-12-30 23:39:08 +0100

    initial changes. includes oh-my-zsh inclusion, deleted unwanted.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias reload!='. ~/.zshrc'
+
+alias say="say --progress"
+
+alias szh='source ~/.zshrc'
+alias tweet='twitter tweet'
+alias byword='open -a Byword'
+alias szh="source ~/.zshrc"
+alias je='killall Safari; killall Chocolat; killall MacVim; killall App\ Store; killall iTunes; killall Xcode;'
+
