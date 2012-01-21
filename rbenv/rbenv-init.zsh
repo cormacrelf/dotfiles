dcd1c90894a85305f27c2e736524970e226ea625
Cormac Relf <chjrelf@gmail.com>
2012-01-22 00:00:38 +1100

    quite frankly no idea since last time

--- rbenv/rbenv-init.zsh
+++ rbenv/rbenv-init.zsh
@@ -1,2 +1,2 @@
 eval "$(rbenv init -)"
-export PATH="$PATH:/Library/Frameworks/Python.framework/Versions/2.7/bin:/Users/Cormac/bin:/Users/Cormac/.rbenv/shims:/Users/Cormac/.rbenv/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin"
+
--- shims/f.zsh
+++ shims/f.zsh
@@ -1,1 +1,8 @@
 source "$DOT/lib/f/f.sh"
+
+alias v='f -e vim' # quick opening files with vim
+alias m='f -e mplayer' # quick opening files with mplayer
+alias j='d -e cd' # quick cd into directories, mimicking autojump and z
+alias o='a -e open' # quick opening files with xdg-open
+
+
