fe72ccb0dd3c534cca728f1948a2c6e17832d3f5
Cormac Relf <cormacrelf@me.com>
2012-02-21 19:10:17 +1100

    updated aliases for _f

--- shims/f.zsh
+++ shims/f.zsh
@@ -4,5 +4,4 @@ alias v='f -e vim' # quick opening files with vim
 alias m='f -e mplayer' # quick opening files with mplayer
 alias j='d -e cd' # quick cd into directories, mimicking autojump and z
 alias o='a -e open' # quick opening files with xdg-open
-
-
+alias s='f -e subl'
