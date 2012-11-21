b05fba574bb67434e6b9df49ff2b2c33f14eb625
Cormac Relf <cormacrelf@me.com>
2012-11-21 12:54:30 +1100

    new rbenv paths

--- rbenv/rbenv-init.zsh
+++ rbenv/rbenv-init.zsh
@@ -1,3 +1,2 @@
-export PATH="/Users/Cormac/.rbenv/shims:/Users/Cormac/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
-export PATH=".:bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$HOME/.cabal/bin:$HOME/Library/Haskell/bin:$DOT/bin:$PATH"
+export PATH=".:bin:$HOME/bin:/usr/local/share/npm/bin:/usr/local/share/python/:/usr/local/bin:/usr/local/sbin:$HOME/.cabal/bin:$HOME/Library/Haskell/bin:$DOT/bin:$PATH"
+
+# GNU coreutils
+PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
