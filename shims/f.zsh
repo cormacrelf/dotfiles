232e01750b62eee03ee13382787f7dc8ef89cdc8
Cormac Relf <cormacrelf@me.com>
2012-11-21 13:08:20 +1100

    fasd updates; aliases and move to homebrew fasd

--- shims/f.zsh
+++ shims/f.zsh
@@ -1,7 +1,7 @@
-eval "$(fasd --init auto)"
+eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp)"
+
 
 alias v='f -e vim' # quick opening files with vim
 alias m='f -e mplayer' # quick opening files with mplayer
-alias j='d -e cd' # quick cd into directories, mimicking autojump and z
+alias j='fasd_cd -d' # quick cd into directories, mimicking autojump and z
 alias o='a -e open' # quick opening files with xdg-open
-alias s='f -e subl'
