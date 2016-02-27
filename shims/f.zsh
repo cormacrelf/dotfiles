78c909e4038472743b3c1a68c9720618e69f8da4
Cormac Relf <cormacrelf@me.com>
2016-02-28 02:33:03 +1100

    Override fasd aliases

--- shims/f.zsh
+++ shims/f.zsh
@@ -1,7 +1,26 @@
-eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp)"
-
-
 alias v='fasd -a -e  vim' # quick opening files with vim
 alias m='fasd -a -e mvim' # quick opening files with mvim
-alias j='fasd_cd -d' # quick cd into directories, mimicking autojump and z
 alias o='a -e open' # quick opening files with xdg-open
+
+alias a='fasd -a'
+alias si='fasd -si'
+alias sid='fasd -sid'
+alias sif='fasd -sif'
+alias d='fasd -d'
+alias f='fasd -f'
+# function to execute built-in cd
+fasd_cd() {
+  if [ $# -le 1 ]; then
+    fasd "$@"
+  else
+    local _fasd_ret="$(fasd -e 'printf %s' "$@")"
+    [ -z "$_fasd_ret" ] && return
+    [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || printf %s\n "$_fasd_ret"
+  fi
+}
+
+# quick cd into directories, mimicking autojump and z
+alias z='fasd_cd -d'
+alias j='fasd_cd -d'
+alias zz='fasd_cd -d -i'
+
