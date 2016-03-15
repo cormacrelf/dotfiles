82ce9006f84c01907f18ad70c515f706dc6453ad
Cormac Relf <cormacrelf@me.com>
2016-03-15 17:52:46 +1100

    shell integration for iterm2

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,10 +3,10 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+    alias ls="gls -F --color"
+    alias l="gls -lAh --color"
+    alias ll="gls -l --color"
+    alias la='gls -A --color'
 fi
 
 alias deactivate-dynamic-pager="sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist"
