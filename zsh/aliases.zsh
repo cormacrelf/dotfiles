2c03bbcd9929b27857ec433f23303fc7534062b9
Cormac Relf <cormacrelf@me.com>
2016-06-16 07:24:07 +1000

    rushed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,27 +1,21 @@
 alias reload!="source $HOME/.zshrc"
 
-alias say="say --progress"
 alias ok="echo 'F.A.B., Thunderbird 2.'"
 alias szh='source ~/.zshrc'
-alias tweet='twitter tweet'
 alias ql='qlmanage -p "$@" >& /dev/null'
 
-alias pat="pygmentize"
 alias bi="brew install"
 
 alias vime="vim -u ~/.vimencrypt -x"
 # Disable capturing of <C-s> by tty
 # alias vim="stty stop '' -ixoff ; vim"
 
-alias pprint="lp -d Photosmart_C309a"
-
 alias vtop="vtop --theme brew"
 
 eval "$(thefuck --alias)"
 alias FUCK='fuck'
+alias fucking='fuck' # fucking bleep
 
 alias curl-resume="curl -C - -L"
-
-alias vim="nvim"
 
 alias statm="stat -c '%a %n'"
