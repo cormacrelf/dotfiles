c2ac2fd499b0f82e06771f67c6766ec9aae073ac
Cormac Relf <cormacrelf@me.com>
2016-08-16 15:13:33 +1000

    antibody: perfection achieved
    - actually installs antibody when it's not there
    - this is because I was using zsh if [[ tests ]]; wrong
    - hash picks up changes in the bundle.zsh file
    - no need for separate bundling of oh-my-zsh, just set envs before
      loading.
    - no need for loading of antibody runtime functions
    - probably could have just used zgen but where's the fun in that

--- antibody/bundle.zsh
+++ antibody/bundle.zsh
@@ -2,35 +2,39 @@ has-antibody () {
     hash antibody 2>/dev/null
 }
 
-if ! [[ has-antibody; ]]; then
+if ! has-antibody; then
     # install it
     sh -s < "$DOT/antibody/antibody-install.sh"
-    if ! [[ has-antibody; ]]; then
+    if ! has-antibody; then
         echo "Failed to install antibody."
         exit
     fi
 fi
 
-
 mkdir -p "$HOME/.antibody"
+bundle_zsh="$DOT/antibody/bundle.zsh"
 ab_plugins="$DOT/antibody/plugins.txt"
 ab_sources="$HOME/.antibody/ab_sources.sh"
 hashes="$HOME/.antibody/hashes.md5"
 
-strip-comments () { grep -v "^\s#\|^\s*$" < "$ab_plugins"; }
+strip-comments () { grep -v "^\s*#\|^\s*$" < "$ab_plugins"; }
 check-sums () { md5sum -c "$hashes" 2>/dev/null >/dev/null;  }
 
-source <(antibody init)
+if [[ ! -e "$ab_sources" ]] || ! check-sums; then
+    echo "regenerating antibodies:"
+    strip-comments | sed 's/^/ - /'
 
+    # Note: antibody here refers to the binary. Once you've $(antibody init)-ed,
+    # you can't pipe bundle output. We won't do this.
+    strip-comments | antibody bundle | sed 's/^/source /' > "$ab_sources"
+
+    # Note: changing this bundle.zsh file will trigger regeneration, for convenience.
+    md5sum "$ab_plugins" "$ab_sources" "$bundle_zsh" > "$hashes"
+fi
+
+# oh-my-zsh overrides, so it knows where it is.
 export DISABLE_AUTO_UPDATE="true"
 export ZSH="$(antibody home)/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh"
 export ZSH_THEME=""
-antibody bundle robbyrussell/oh-my-zsh
-
-if [[ ! -e "$ab_sources" || ! check-sums; ]]; then
-    rm -f "$ab_sources"
-    strip-comments | antibody bundle | xargs -I {} echo "source {}" >> "$ab_sources"
-    md5sum "$ab_plugins" "$ab_sources" > "$hashes"
-fi
 
 source $ab_sources
