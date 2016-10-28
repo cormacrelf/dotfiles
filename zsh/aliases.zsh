3001ee463d525b75705c90e6e455bcdb9e92c951
Cormac Relf <cormacrelf@me.com>
2016-10-29 02:12:30 +1100

    RUST_SRC_PATH instead of manual rust repo clone

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,23 +1,15 @@
 alias reload!="source $HOME/.zshrc"
-
-alias ok="echo 'F.A.B., Thunderbird 2.'"
 alias szh='source ~/.zshrc'
 alias ql='qlmanage -p "$@" >& /dev/null'
-
 alias bi="brew install"
-
-alias vime="vim -u ~/.vimencrypt -x"
+alias bu="brew upgrade"
 # Disable capturing of <C-s> by tty
 # alias vim="stty stop '' -ixoff ; vim"
-
 alias vtop="vtop --theme brew"
-
 eval "$(thefuck --alias)"
 alias FUCK='fuck'
 alias fucking='fuck' # fucking bleep
-
 alias curl-resume="curl -C - -L"
-
 alias statm="stat -c '%a %n'"
-
 alias g=git
+alias weather="ansiweather -l Canberra -a false"
