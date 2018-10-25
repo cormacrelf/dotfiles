b4d9851615e4bc39dc0b35c7a1803d4c2e9d277a
Cormac Relf <web@cormacrelf.net>
2018-10-25 14:29:06 +1100

    bork: some shortcuts maybe later

---
+++ bork/mac/shortcuts.sh
@@ -0,0 +1,5 @@
+# http://hints.macworld.com/article.php?story=20131123074223584
+# The meta-keys are set as @ for Command, $ for Shift, ~ for Alt and ^ for Ctrl. k in this example is the non-meta-key that you want to use.
+
+# defaults write com.culturedcode.Things3 NSUserKeyEquivalents -dict-add "Menu Item" -string "@$~^k"
+
